f40ff61983bc27d4bd1f739363ce69c2d689b736
ChristopherZinger <krzysztof.zinger@gmail.com>
2020-10-03 22:38:07 +0200

    remove leftovers

--- src/components/Experience/ExperienceLayout/ExperienceLayout.js
+++ src/components/Experience/ExperienceLayout/ExperienceLayout.js
@@ -3,26 +3,13 @@ import ExperienceList from '../ExperienceList/ExperienceList';
 
 const ExperienceLayout = () => {
     return (
-        <ExperienceList />
+        <div>
+            <h1>Experience:</h1>
+            <ExperienceList />
+        </div>
+
     )
 }
 
-
-
-// const mapStateToProps = state => {
-//     return {
-//         positions: state.experienceReducers.positions,
-//         isLoading: state.experienceReducers.isLoading,
-//     }
-// }
-
-
-// const mapDispatchToProps = dispatch => {
-//     return {
-//         getExperience: () => dispatch(actions.getExperience()),
-//         removeExperience: (id) => dispatch(actions.removeExperience(id)),
-//     }
-// }
-// connect(mapStateToProps, mapDispatchToProps)(ExperienceList)
 
 export default ExperienceLayout;
