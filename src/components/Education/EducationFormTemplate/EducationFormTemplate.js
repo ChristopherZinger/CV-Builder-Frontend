5fa42fe87a061bb52077ed8b15217eabf1cbda6d
ChristopherZinger <krzysztof.zinger@gmail.com>
2020-10-04 13:12:46 +0200

    create EducationFormTemplate component

---
+++ src/components/Education/EducationFormTemplate/EducationFormTemplate.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import EducationForm from '../EducationForm/EducationForm';
+import * as gs from '../../_globalStyles/globalStyles.module.css';
+
+
+const EducationFormTemplate = props => {
+    return (
+        <div className={gs.myContainer} >
+            <EducationForm data={{}} clearOnSave={props.clearOnSave}>
+                <button onClick={props.clickCancel} className={[gs.btn, gs.btnDisabled].join(' ')} > cancel </button>
+            </EducationForm>
+        </div>
+    )
+}
+
+export default EducationFormTemplate;
